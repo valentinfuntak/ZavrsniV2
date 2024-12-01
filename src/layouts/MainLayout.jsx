1bbf9aa82601df0126164de1a72dfec725cdc7b5
valentinfuntak <valentinfuntakk@gmail.com>
2024-12-01 17:02:11 +0100

    Dorada footera

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -202,7 +202,7 @@ export default function MainLayout(props) {
                     </div>
                     <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
                     <div class="sm:flex sm:items-center sm:justify-between">
-                        <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.
+                        <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© {new Date().getFullYear()}<a href="https://ss-obrtnicka-koprivnica.skole.hr/" class="hover:underline"> OBSCK™</a> Sva prada podržana.
                         </span>
                         <div class="flex mt-4 sm:justify-center sm:mt-0">
                             <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
