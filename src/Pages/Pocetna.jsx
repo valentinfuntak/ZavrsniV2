92906750985ea2ce04bfe3ab6242de7040ed2685
valentinfuntak <valentinfuntakk@gmail.com>
2024-12-24 13:43:59 +0100

    Dodane engefunckije

--- src/Pages/Pocetna.jsx
+++ src/Pages/Pocetna.jsx
@@ -1,14 +1,20 @@
+import FetchMessage from "../Components/Funkcija";
 function Pocetna() {
     return (
-        <div class="min-h-screen bg-gradient-to-r from-blue-600 via-blue-500 to-blue-400 flex flex-col justify-center items-center text-white">
-            <div class="text-center p-6">
-                <h1 class="font-mono text-5xl font-bold mb-4">Dobrodošli na Planespotter</h1>
-                <p class="text-lg mb-8">
-                    Ova aplikacija omogućava skeniranje neba i praćenje aviona u stvarnom vremenu.
-                    Saznajte više o letovima u vašem području!
-                </p>
+        <>
+            <div class="min-h-screen bg-gradient-to-r from-blue-600 via-blue-500 to-blue-400 flex flex-col justify-center items-center text-white">
+                <div class="text-center p-6">
+                    <h1 class="font-mono text-5xl font-bold mb-4">Dobrodošli na Planespotter</h1>
+                    <p class="text-lg mb-8">
+                        Ova aplikacija omogućava skeniranje neba i praćenje aviona u stvarnom vremenu.
+                        Saznajte više o letovima u vašem području!
+                    </p>
+                </div>
             </div>
-        </div>
+            <div>
+                <FetchMessage />
+            </div>
+        </>
     );
 }
 
