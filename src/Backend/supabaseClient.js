40df362248a05c26bd79b330b2096cf47d8ae623
valentinfuntak <valentinfuntakk@gmail.com>
2024-12-25 11:52:25 +0100

    Updates

--- src/Backend/supabaseClient.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const url = import.meta.env.VITE_SUPABASE_URL;
-const apiKey = import.meta.env.VITE_SUPABASE_API_KEY;
-
-const supabase = createClient(url, apiKey);
-
-export default supabase;
