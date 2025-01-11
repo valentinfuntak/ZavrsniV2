71665a0742b61b2fe46c863177eba41c18dd8d18
valentinfuntak <valentinfuntakk@gmail.com>
2025-01-11 20:22:35 +0100

    Doradenje stranice

--- src/Pages/Informacije.jsx
+++ src/Pages/Informacije.jsx
@@ -1,21 +1,33 @@
-import { createSignal } from "solid-js";
-
 function Informacije(props) {
   return (
-    <div class="p-6 bg-white dark:bg-gray-800 shadow-lg rounded-lg max-w-4xl mx-auto dark:text-white border-4 border-gray-300 dark:border-gray-600 hover:border-blue-500 transition-all">
-      <h1 class="font-inter text-4xl font-semibold mb-4">INFORMACIJE O APLIKACIJI</h1>
-      <p class="text-lg text-gray-700 dark:text-gray-300 mb-4">
-        Ova aplikacija koristi <a href="https://www.flightradar24.com/" class="text-blue-500 hover:underline" target="_blank" rel="noopener noreferrer">Flightradar24 API</a> za skeniranje neba i prikazivanje informacija o avionima u stvarnom vremenu. Korištenjem najnovijih podataka o letovima, omogućuje korisnicima praćenje avionskih ruta, visine, brzine i ostalih važnih parametara. Aplikacija omogućuje jednostavno pretraživanje i filtriranje letova na temelju različitih kriterija.
-      </p>
-      <p class="text-lg text-gray-700 dark:text-gray-300 mb-4">
-        <a href="https://solidjs.com/" class="text-blue-500 hover:underline" target="_blank" rel="noopener noreferrer">SolidJS</a> je korišten za optimalne performanse aplikacije, dok su za dizajn korišteni <a href="https://tailwindcss.com/" class="text-blue-500 hover:underline" target="_blank" rel="noopener noreferrer">TailwindCSS</a> i <a href="https://flowbite.com/" class="text-blue-500 hover:underline" target="_blank" rel="noopener noreferrer">Flowbite</a>, što omogućava moderni izgled i jednostavno korisničko iskustvo.
-      </p>
-      <p class="text-lg text-gray-700 dark:text-gray-300">
-        Flightradar24 je jedan od najpoznatijih globalnih servisa za praćenje letova, a kroz našu aplikaciju, korisnici mogu uživati u brzom pristupu podacima o avionima koji lete u njihovoj blizini ili bilo kojem dijelu svijeta. Informacije o svakom letu uključuju podatke o tipu aviona, vremenu polijetanja, odredištu, brzini leta i drugim detaljima koji mogu biti korisni za ljubitelje avijacije ili putnike.
-      </p>
-      <p class="text-lg text-gray-700 dark:text-gray-300">
-        Aplikacija je dizajnirana uz pomoć SolidJS-a za optimalne performanse, dok su za dizajn korišteni TailwindCSS i Flowbite, što omogućava moderni izgled i jednostavno korisničko iskustvo.
-      </p>
+    <div class="p-8">
+      <h1 class="font-mono text-4xl mb-6">INFORMACIJE O APLIKACIJI</h1>
+
+      <div class="space-y-6">
+        <div>
+          <p>
+            Ova aplikacija koristi <a href="https://www.flightradar24.com/" class="text-blue-500 hover:underline" target="_blank" rel="noopener noreferrer">Flightradar24 API</a> za skeniranje neba i prikazivanje informacija o avionima u stvarnom vremenu. Korištenjem najnovijih podataka o letovima, omogućuje korisnicima praćenje avionskih ruta, visine, brzine i ostalih važnih parametara. Aplikacija omogućuje jednostavno pretraživanje i filtriranje letova na temelju različitih kriterija.
+          </p>
+        </div>
+
+        <div>
+          <p>
+            <a href="https://solidjs.com/" class="text-blue-500 hover:underline" target="_blank" rel="noopener noreferrer">SolidJS</a> je korišten za optimalne performanse aplikacije, dok su za dizajn korišteni <a href="https://tailwindcss.com/" class="text-blue-500 hover:underline" target="_blank" rel="noopener noreferrer">TailwindCSS</a> i <a href="https://flowbite.com/" class="text-blue-500 hover:underline" target="_blank" rel="noopener noreferrer">Flowbite</a>, što omogućava moderni izgled i jednostavno korisničko iskustvo.
+          </p>
+        </div>
+
+        <div>
+          <p class="text-lg text-gray-700 dark:text-gray-300">
+            Flightradar24 je jedan od najpoznatijih globalnih servisa za praćenje letova, a kroz našu aplikaciju, korisnici mogu uživati u brzom pristupu podacima o avionima koji lete u njihovoj blizini. Informacije o svakom letu uključuju podatke o tipu aviona, visini, poziciji, brzini leta i drugim detaljima.
+          </p>
+        </div>
+
+        <div>
+          <p class="text-lg text-gray-700 dark:text-gray-300">
+            Aplikacija je dizajnirana uz pomoć SolidJS-a za optimalne performanse, dok su za dizajn korišteni TailwindCSS i Flowbite, što omogućava moderni izgled i jednostavno korisničko iskustvo.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
--- src/Pages/Onama.jsx
+++ src/Pages/Onama.jsx
@@ -1,37 +1,43 @@
-import { createSignal } from "solid-js";
 import Logo from "../assets/logo.png";
 
 function Onama(props) {
     return (
-        <div class="min-h-screen bg-gray-900 text-white p-8 rounded-lg">
-            <div class="max-w-3xl mx-auto bg-gray-800 shadow-lg rounded-lg p-6">
-                <div class="flex justify-center mb-6">
-                    <img src={Logo} alt="Logo" class="w-32 h-auto"/>
-                </div>
-                <h1 class="font-mono text-4xl font-bold text-center text-gray-100 mb-6">O NAMA</h1>
+        <div class="p-8">
+            <h1 class="font-mono text-4xl mb-6">O NAMA</h1>
 
-                <div class="space-y-12">
-                    <section>
-                        <h2 class="text-2xl font-semibold text-gray-100 mb-4">Škola</h2>
-                        <p class="text-lg text-gray-300">
-                            Obrtnička škola Koprivnica
-                        </p>
-                    </section>
-                    <section>
-                        <h2 class="text-2xl font-semibold text-gray-100 mb-4">Mentor</h2>
-                        <p class="text-lg text-gray-300">
+            <div class="space-y-6">
+                <div class="flex justify-center mb-10">
+                    <img
+                        src={Logo}
+                        alt="Logo"
+                        class="w-40 h-auto rounded-full shadow-md transform hover:scale-110 transition-transform duration-300"
+                    />
+                </div>
+                <div>
+                    <h2 class="text-xl font-semibold">Škola</h2>
+                    <p>
+                        Obrtnička škola Koprivnica
+                    </p>
+                </div>
+                <div>
+                    <h2 class="text-xl font-semibold">Mentor</h2>
+                    <p>
                         Tomislav Jakupić
-                        </p>
-                    </section>
-                    <section>
-                        <h2 class="text-2xl font-semibold text-gray-100 mb-4">Učenici</h2>
-                        <p class="text-lg text-gray-300">
-                            Valentin Funtak
-                        </p>
-                        <p class="text-lg text-gray-300">
-                            Noa Golubić
-                        </p>
-                    </section>
+                    </p>
+                </div>
+
+                <div>
+                    <h2 class="text-xl font-semibold">Učenici</h2>
+                    <p class="text-lg text-gray-700 dark:text-gray-300">
+                        Valentin Funtak <br />
+                        Noa Golubić
+                    </p>
+                </div>
+
+                <div>
+                    <p class="text-lg text-gray-700 dark:text-gray-300">
+
+                    </p>
                 </div>
             </div>
         </div>
