5115dbfb699dd0d51362b0251b47babcb32193eb
valentinfuntak <valentinfuntakk@gmail.com>
2025-01-12 18:42:10 +0100

    .

--- src/Pages/Postavke.jsx
+++ src/Pages/Postavke.jsx
@@ -1,9 +1,4 @@
-import { createSignal } from "solid-js";
-import Plane from "../assets/planefav.png";
-
 function Postavke(props) {
-
-
     return (
         <div class="max-w-screen-lg mx-auto p-6">
             <h1 class="font-mono text-4xl mb-3">POSTAVKE</h1>
---
+++ src/Pages/Racun.jsx
@@ -0,0 +1,9 @@
+function Racun(props) {
+    return (
+        <div class="max-w-screen-lg mx-auto p-6">
+            <h1 class="font-mono text-4xl mb-3">RACUN</h1>
+        </div>
+    );
+}
+
+export default Racun;
