a05638dc9cb05fa06fb35e32e49982626e3cb09f
valentinfuntak <valentinfuntakk@gmail.com>
2025-01-16 19:09:14 +0100

    .

--- src/Pages/Program.jsx
+++ src/Pages/Program.jsx
@@ -33,7 +33,7 @@ function Program(props) {
 
     return (
         <>
-            {!isMobile() && <Navigacija />}
+            {isMobile() && <Navigacija />}
             <div class="mt-5 flex flex-column sm:flex-row flex-wrap space-y-4 sm:space-y-0 items-center justify-between pb-4">
                 <div>
                     <button id="dropdownRadioButton" data-dropdown-toggle="dropdownRadio" class="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" type="button">
