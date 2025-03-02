132400c8665b8803e8025589af6af9041532a517
valentinfuntak <valentinfuntakk@gmail.com>
2025-03-02 12:25:07 +0100

    Updates

---
+++ assets/browser-BTma8349.js
@@ -0,0 +1,1 @@
+import{g as e}from"./index-CWs-6Jyr.js";var o=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")};const r=e(o),s=Object.freeze(Object.defineProperty({__proto__:null,default:r},Symbol.toStringTag,{value:"Module"}));export{s as b};
