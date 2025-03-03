9b89561e950739e2913f6e9268508437f8f01852
valentinfuntak <valentinfuntakk@gmail.com>
2025-03-03 20:20:40 +0100

    .

--- src/Pages/Funkcija.jsx
+++ src/Pages/Funkcija.jsx
@@ -9,14 +9,14 @@ const Funkcija = () => {
   const handleClick = async () => {
     try {
       const { data, error } = await supabase.functions.invoke("Search", {
-        body: { name: "Functions" },  
+        body: { name: "Functions" },
       });
 
       if (error) {
         console.error("Greška pri pozivu funkcije:", error.message);
         setMessage("Došlo je do greške pri pozivu funkcije.");
       } else {
-        setMessage(data.message); 
+        setMessage(data.message);
         console.log("Odgovor funkcije:", data.message);
       }
     } catch (err) {
@@ -28,8 +28,8 @@ const Funkcija = () => {
   return (
     <div>
       <h1>Supabase Edge Funkcija</h1>
-      <button 
-        onClick={handleClick} 
+      <button
+        onClick={handleClick}
         class="bg-blue-500 text-white p-2 rounded-md"
       >
         Pozovi funkciju
